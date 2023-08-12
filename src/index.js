c414d560fbb4b9cf2d64e9d8fa6fe52f238b4698
gauraj <gaurajrakholiya@gmail.com>
2023-08-12 15:19:03 +0530

    react setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 
-import {a,b} from './app'
+// import {a,b} from './app'
 
-console.log(a)
-console.log(b )
+// import './style.css'
+// import './style1.css'
+
+// console.log(a)
+// console.log(b)
 
 
 // dependencies no  vapratu hoy te
 // devdependencies program ma vapratu hoy
 // 
+
+
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<h1> hello world!</h1>);
